e9a159c5ae141d83091289a5342b1f569effe353
JonatanLF <jonatan@heimo.no>
2024-05-30 11:14:37 +0200

    Dijkstra + bilkommando

--- SmartCityMP-main/Den store dijkstrakode V2.py
+++ SmartCityMP-main/Den store dijkstrakode V2.py
@@ -5,8 +5,10 @@ bom_verdi =  [2, 1, 4, 5, 2, 4, 8, 9, 2, 5, 3, 3, 5, 7]
 punkter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 ekstremalpunkter = ['G', 'H']
 
-start = 'H'
-stopp = 'G'
+start = 'G'
+stopp = 'H'
+
+kart1 = []
 
 linjer = [
     ['A', 'B', bom_verdi[0]],
@@ -95,9 +97,42 @@ def dijkstra(punkter, linjer, start, stopp):
     kart1 = kart[::-1]           
     return kart1, total_lengde
     
-print(dijkstra(punkter, linjer, start, stopp))
+intKart = []
+dirKart = []
 
+for i in dijkstra(punkter, linjer, start, stopp)[0]:
+    if(i == 'A' or i == 'F'):
+        intKart.append(2)
+    if(i == 'B' or i == 'E'):
+        intKart.append(3)
+    if(i == 'C' or i == 'D'):
+        intKart.append(1)
+    if(i == 'G' or i == 'H'):
+        intKart.append(4)
 
+dirKart.append(intKart[1] - intKart[2])
+if(intKart[3] == 1 and intKart[2] != 1):
+    dirKart.append(3)
+    if(intKart[4] == 3):
+        dirKart.append(2)
+        dirKart.append(2)
+    else:
+        dirKart.append(3)
+        dirKart.append(1)
+        dirKart.append(1)
+        dirKart.append(3)
+
+else:
+    dirKart.append(intKart[3])
+    if(intKart[3] == 2):
+        dirKart.append(1)
+        dirKart.append(3)
+    else:
+        dirKart.append(3)
+        dirKart.append(2)
+        
+print(dirKart)
+print(dijkstra(punkter, linjer, start, stopp)[1])
 
 
 
